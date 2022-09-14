cc8085bc4b45ad34ca5495ad14f56a825a9561d9
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-14 11:04:16 +0300

    Variables (class fields) and constructor (overloaded) are added to the Ball class

--- src/Ball/Ball.java
+++ src/Ball/Ball.java
@@ -2,6 +2,18 @@ package Ball;
 
 public class Ball {
   // Variables
-  // Constructor
+  private double x;
+  private double y;
+
+  // Constructor (overloaded)
+  public Ball() {
+    x = 0.0;
+    y = 0.0;
+  }
+  public Ball(double newX, double newY) {
+    x = newX;
+    y = newY;
+  }
+
   // Methods
 }
