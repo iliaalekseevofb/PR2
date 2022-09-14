232100c99aceeeb4f60142775c38b61238a08e26
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-14 11:08:58 +0300

    Methods are added to the class Ball

--- src/Ball/Ball.java
+++ src/Ball/Ball.java
@@ -16,4 +16,27 @@ public class Ball {
   }
 
   // Methods
+  public double getX() {
+    return this.x;
+  }
+  public double getY() {
+    return this.y;
+  }
+  public void setX(double newX) {
+    this.x = newX;
+  }
+  public void setY(double newY) {
+    this.y = newY;
+  }
+  public void setXY(double newX, double newY) {
+    this.x = newX;
+    this.y = newY;
+  }
+  public void move(double xDisp, double yDisp) {
+    this.x += xDisp;
+    this.y += yDisp;
+  }
+  public String toString() {
+    return "X position: " + this.x + ", Y position: " + this.y;
+  }
 }
